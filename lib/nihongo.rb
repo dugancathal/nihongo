f1c34478742e48d5acfc90e0d8204f60dcdb2365
TJ Taylor <dugancathal@gmail.com>
2026-01-09 21:40:36 -0700

    Add "logger" to nihongo module

--- lib/nihongo.rb
+++ lib/nihongo.rb
@@ -6,6 +6,10 @@ require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem
 
 module Nihongo
+  def self.logger = @logger || $stderr
+  def self.logger=(other)
+    @logger = other
+  end
 end
 
 loader.setup
