e7b535f7cea152d4534aa9bdc0682ea1b0e9603c
TJ Taylor <dugancathal@gmail.com>
2026-01-10 00:02:26 -0700

    Support reverse reviews on cards

--- lib/nihongo/mochi/card.rb
+++ lib/nihongo/mochi/card.rb
@@ -1,8 +1,9 @@
 module Nihongo
   module Mochi
-    class Card < Data.define(:content, :deck_id, :id, :name, :pos, :reviews, :created_at)
+    class Card < Data.define(:content, :deck_id, :id, :name, :pos, :reviews, :reverse_reviews, :created_at)
       def self.parse(transit_json)
         reviews = Array(transit_json["~:reviews"]).map { Mochi::Review.parse(_1) }
+        reverse_reviews = Array(transit_json["~:reverse-reviews"]).map { Mochi::Review.parse(_1) }
         self.new(
           content: transit_json["~:content"],
           deck_id: transit_json["~:deck_id"],
@@ -11,11 +12,21 @@ module Nihongo
           name: transit_json["~:name"],
           pos: transit_json["~:pos"],
           reviews:,
+          reverse_reviews:
         )
       end
 
-      def initialize(content:, deck_id: nil, id: nil, name: nil, pos: nil, reviews: [], created_at: Time.now.utc.round(0))
-        super(content:, deck_id:, id:, name:, pos:, reviews:, created_at:)
+      def initialize(
+        content:,
+        deck_id: nil,
+        id: nil,
+        name: nil,
+        pos: nil,
+        reviews: [],
+        reverse_reviews: [],
+        created_at: Time.now.utc.round(0)
+      )
+        super
       end
 
       def as_transit_json
@@ -23,6 +34,7 @@ module Nihongo
           "~:content": content,
           "~:id": TransitJson.keywordify(id),
           "~:reviews": reviews.map(&:as_transit_json),
+          "~:reverse-reviews": reverse_reviews.map(&:as_transit_json),
           "~:created-at": { "~#dt": created_at.to_i * 1000 },
         }.merge({
           "~:deck_id": deck_id,
